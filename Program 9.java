832ee5647111d583536df03f7dbdb33bec49edc3
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-11 16:14:01 +0530

    Update Program 9.java

--- Program 9.java
+++ Program 9.java
@@ -1,5 +1,5 @@
 Example of Method Overloading by changing data type of argument
-	class Calculation2{  
+	public class Calculation2{  
 	  void sum(int a,int b){System.out.println(a+b);}  
 	  void sum(double a,double b){System.out.println(a+b);}  
 	  
