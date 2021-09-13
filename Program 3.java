066abe749c82c6d40aee4683ce75101cc402aa18
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-13 08:36:46 +0530

    Update Program 3.java

--- Program 3.java
+++ Program 3.java
@@ -13,8 +13,8 @@
 	  Rectangle r1=new Rectangle();  
 	  Rectangle r2=new Rectangle();  
 	  
-	  r1.insert(11,5);  
-	  r2.insert(3,15);  
+	  r1.insert(10,4);  
+	  r2.insert(7,12);  
 	  
 	  r1.calculateArea();  
 	  r2.calculateArea();  
