6f97787e4cdf442fe037206618f559401a4d3918
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-13 08:42:17 +0530

    Update program 7.java

--- program 7.java
+++ program 7.java
@@ -13,8 +13,8 @@ public class Rectangle{
 	 public static void main(String args[]){  
   	  Rectangle r1=new Rectangle(),r2=new Rectangle();//creating two objects  
 	    
-	  r1.insert(11,5);  
-	  r2.insert(3,15);  
+	  r1.insert(7,3);  
+	  r2.insert(14,20);  
 	  
 	  r1.calculateArea();  
 	  r2.calculateArea();  
