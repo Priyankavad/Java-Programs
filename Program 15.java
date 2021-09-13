3c8b2bc82f0efd26210929c9fb8ace45f253925b
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-13 08:54:27 +0530

    Update Program 15.java

--- Program 15.java
+++ Program 15.java
@@ -13,7 +13,7 @@
 	    void display(){System.out.println(id+" "+name);}  
   	   
   	  public static void main(String args[]){  
-	    Student6 s1 = new Student6(111,"Karan");  
+	    Student6 s1 = new Student6(471,"Hemnaath");  
 	    Student6 s2 = new Student6(s1);  
 	    s1.display();  
 	    s2.display();  
