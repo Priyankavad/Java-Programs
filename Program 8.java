27d3d94fffc2f3f0dd070e22112b1263a3cf6def
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-13 08:45:39 +0530

    Update Program 8.java

--- Program 8.java
+++ Program 8.java
@@ -1,12 +1,12 @@
-Example of Method Overloading by changing the no. of arguments
+//Example of Method Overloading by changing the no. of arguments
 	public class Calculation{  
 	  void sum(int a,int b){System.out.println(a+b);}  
 	  void sum(int a,int b,int c){System.out.println(a+b+c);}  
 	  
 	  public static void main(String args[]){  
 	  Calculation obj=new Calculation();  
-	  obj.sum(10,10,10);  
-	  obj.sum(20,20);  
+	  obj.sum(70,90,80);  
+	  obj.sum(60,20);  
 	  
 	  }  
 	}  
