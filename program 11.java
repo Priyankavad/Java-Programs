0bf428887bdd92eae97dba9e82a19694350131b3
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-13 08:50:19 +0530

    Update program 11.java

--- program 11.java
+++ program 11.java
@@ -1,6 +1,6 @@
-Java Default Constructor
-	class Bike1{  
-	    Bike1(){System.out.println("Bike is created");}  
+//Java Default Constructor
+	public class Bike1{  
+	    Bike1(){System.out.println("Bike is operated");}  
 	public static void main(String args[]){  
 	    Bike1 b=new Bike1();  
 	}  
