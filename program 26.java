96af3255d1f490751409ac2f608567e3508886ab
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-18 15:23:20 +0530

    Create program 26.java

---
+++ program 26.java
@@ -0,0 +1,13 @@
+//Example of this keyword that you return as a statement from the method
+	class A{  
+	A getA(){  
+	return this;  
+	}  
+	void msg(){System.out.println("Hiiii java");}  
+	}  
+	  
+	public class Test1{  
+	public static void main(String args[]){  
+	new A().getA().msg();  
+	}  
+	}  
