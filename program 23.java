dc79dba55556adb9efdb9b5d4663a2326ec2a8dd
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-18 15:09:24 +0530

    Create program 23.java

---
+++ program 23.java
@@ -0,0 +1,16 @@
+// this keyword can be used to invoke current class method (implicitly).
+	public class H{  
+	  void m(){  
+	  System.out.println("method is invoked");  
+	  }  
+	  void n(){  
+	  this.m();//no need because compiler does it for you.  
+	  }  
+	  void p(){  
+	  n();//complier will add this to invoke n() method as this.n()  
+	  }  
+	  public static void main(String args[]){  
+	  H s1 = new H();  
+	  s1.p();  
+	  }  
+	}  
